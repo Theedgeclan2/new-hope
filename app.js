363ecf387fd0d7ff88cf658443d0443b5252161f
Unknowen <noahprice2016@gmail.com>
2018-12-20 19:20:25 +0800

    Update app.js

--- app.js
+++ app.js
@@ -112,4 +112,7 @@ client.on('error', function (error) {
 
 client.login(process.env.token || config.token);
 
-
+var http = require("http");
+setInterval(function() {
+    http.get("https://a-new-hope-1.herokuapp.com");
+}, 300000); // every 5 minutes (300000)
